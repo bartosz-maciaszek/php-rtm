63ee7e14e9cd8e45f5980d03c9f1c7e1d8d9494e
Bartosz Maciaszek <bartosz.maciaszek@netart.pl>
2013-04-19 15:08:59 +0200

    Add MIT licesnse information, remove RtmInterface

--- src/Rtm/Client.php
+++ src/Rtm/Client.php
@@ -1,4 +1,34 @@
 <?php
+/**
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2013 Bartosz Maciaszek <bartosz.maciaszek@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package    Rtm
+ * @author     Bartosz Maciaszek <bartosz.maciaszek@gmail.com>
+ * @copyright  2013 Bartosz Maciaszek.
+ * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
+ */
 
 namespace Rtm;
 
@@ -12,11 +42,22 @@ class Client implements ClientInterface
      */
     private $rtm = null;
 
-    public function setRtm(RtmInterface $rtm)
+    /**
+     * Set Rtm object
+     * @param Rtm $rtm
+     */
+    public function setRtm(Rtm $rtm)
     {
         $this->rtm = $rtm;
     }
 
+    /**
+     * Makes a request to RTM API
+     * @param  string $method
+     * @param  array  $params
+     * @return DataContainer
+     * @throws Rtm\Exception If response is not valid
+     */
     public function call($method, array $params = array())
     {
         $request = new Request($params);
